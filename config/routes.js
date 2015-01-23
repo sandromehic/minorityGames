53cd83619f247d5668f490dbfb04347cb7f23eff
sandromehic <sandromehic@gmail.com>
2015-01-23 19:19:28 +0100

    added route and layout for classical minority game, added angular to project

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 module.exports = function(app) {
-    app.get('/', function(req, res) {
+    app.get('/', function (req, res) {
     	res.render('index');
+    });
+    app.get('/classicalminority', function (req, res) {
+    	res.render('classicalminority');
     });
 };
--- socket-logic.js
+++ socket-logic.js
@@ -2,5 +2,37 @@ exports = module.exports = function(io) {
 	// create a socket for every active connection and listen to other events
 	io.sockets.on('connection', function (socket) {
 		console.log('user connected');
+		
+		// --- EVENTS
+		// disconnect event
+		socket.on('disconnect', function (data) {
+			console.log('a user disconnected');
+		});
+
+		// get graph, client can require graph information
+		// it should however all be sent on every new round
+		socket.on('getGraph', function (data) {
+			console.log('user requested graph data!');
+		});
+
+		// get score (same as above)
+		socket.on('getScore', function (data) {
+			console.log('user requested score data!');
+		});
+
+		// get rank (same as above)
+		socket.on('getRank', function (data) {
+			console.log('user requested rank data!');
+		});
+
+		// get rank difference (same as above)
+		socket.on('getRankDifference', function (data) {
+			console.log('user requested rank difference data!');
+		});
+
+		// get neighbors (same as above)
+		socket.on('getNeighbors', function (data) {
+			console.log('user requested neighbors data!');
+		});
 	});
 }
