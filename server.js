e4fb6a20f598488ca10cffb772975e2e36dcc2b8
sandromehic <sandromehic@gmail.com>
2015-04-09 11:00:06 +0200

    added sockets.io heroku requiremente

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ app.use(cookieParser());
 // set public folder from which client side scripts will be loaded
 app.use(express.static(__dirname + '/public'));
 
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 // routing
 require('./config/routes.js')(app);
 
